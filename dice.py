0c81775eb299be3f823b66b8a3b477a1857fdd0c
bwsttvdev <97367442+bwsttvdev@users.noreply.github.com>
2022-12-08 21:52:35 +1030

    New code
    Better code

--- dice.py
+++ dice.py
@@ -1,8 +1,46 @@
-import random
+class Dice:
+    def __init__(self):
+        self.sides = 20
+ 
+    def roll(self):
+        import random
+        number = random.randint(1, self.sides)
+        return number
+ 
+d20 = Dice()
+print(d20.roll())
+ 
+class D4(Dice):
+    def __init__(self):
+        self.sides = 4
 
+d4 = D4()
+print(d4.roll())
+ 
+class D6(Dice):
+    def __init__(self):
+        self.sides = 6
 
-def d20():
-    return random.randint(1, 20)
+d6 = D6()
+print(d6.roll())
+ 
+class D8(Dice):
+    def __init__(self):
+        self.sides = 8
 
-def d6():
-    return random.randint(1, 6)
+d8 = D8()
+print(d8.roll())
+ 
+class D10(Dice):
+    def __init__(self):
+        self.sides = 10
+
+d10 = D10()
+print(d10.roll())
+ 
+class D12(Dice):
+    def __init__(self):
+        self.sides = 12
+
+d12 = D12()
+print(d12.roll())
